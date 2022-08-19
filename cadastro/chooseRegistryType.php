fd19f93d304e3b825671514a5b16e1f8c7b643bf
Pedro Gonçalves <pedrgonmoreira@gmail.com>
2022-08-19 14:25:37 -0300

    repairing test commit

--- cadastro/chooseRegistryType.php
+++ cadastro/chooseRegistryType.php
@@ -4,7 +4,7 @@
 <h1>Selecione qual o tipo de cadastro a ser realizado: </h1>
 <a href="user-registry/userRegistry.php"><img src="/login/user-login/img/usuario.png" height="250" width="250"></a>
 <br>
-<label>Usuári</label>
+<label>Usuário</label>
 <br><br>
 <a href="enterprise-registry/enterpriseRegistry.php"><img src="/login/user-login/img/office.png" height="250" width="250"></a>
 <br>
