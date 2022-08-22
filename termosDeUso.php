bb10d0c0f03d1f2e539bb2bcff998111cc6d3251
liniker <linikergabrieldasilva@gmail.com>
2022-08-22 16:03:14 -0300

    feat: correcting href of userTerms

--- termosDeUso.php
+++ termosDeUso.php
@@ -97,7 +97,7 @@ Apenas retemos as informações coletadas pelo tempo necessário para fornecer o
 
 Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.<br>
 
-O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a href="file:///C:/Users/victor.hlsilva3/Desktop/site/politica.termos.html"> políticas de privacidade.</a><br><br>
+O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a href="termosDeUso.php"> políticas de privacidade.</a><br><br>
 
 Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.
 
